41d9679ad1f0d16055ad1a0c98922627cef752d9
myyk <myyk@egraphs.com>
2012-08-23 16:47:21 -0700

    Todolist index.html now can show the myyk.jpg from subProject1

--- mainProject/project/Build.scala
+++ mainProject/project/Build.scala
@@ -11,7 +11,12 @@ object ApplicationBuild extends Build {
     "org.squeryl" %% "squeryl" % "0.9.5-2",
     "postgresql" % "postgresql" % "9.1-901-1.jdbc4")
 
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings( // Add your own project settings here      
+  val subProject1 = PlayProject(
+    "subProject1", appVersion, path = file("modules/subProject1")
   )
+
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      // Add your own project settings here      
+  ).dependsOn(subProject1)
 
 }
